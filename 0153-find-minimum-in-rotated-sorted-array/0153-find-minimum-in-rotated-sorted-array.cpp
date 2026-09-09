cc381e12641feaf74adfa038ce706938565648d8
Aman <amankrr.info@gmail.com>
2026-09-09 18:53:05 +0530

    add comment

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,11 +4,12 @@ public:
         int st = 0 , end = nums.size()-1;
 
         while(st < end){
-            int mid = st + (end - st)/2;
+            int mid = st + (end - st)/2; 
 
-            if(nums[mid] < nums[end]){
-                end = mid;
-            }else{
+            if(nums[mid] < nums[end]){ //min is in the left half
+                end = mid;    //because nums[mid] could itself be the minimum element;
+        
+            }else{            //min is at the right half;
                 st = mid +1;
             }
         }
